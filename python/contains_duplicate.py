4beb1d3258b880925d43d47421dbba3fd0e5465e
Cameron <cdart2@illinois.edu>
2019-03-17 14:21:44 -0700

    Update contains_duplicate.py

--- python/contains_duplicate.py
+++ python/contains_duplicate.py
@@ -1,3 +1,9 @@
+"""
+Idea: Create a set to remove duplicates and compare that length to the length of the input array.
+
+Time: O(n)
+Space: O(n)
+"""
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         return len(set(nums)) != len(nums)
